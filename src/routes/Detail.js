01c3063d5889b79183642725ec1c46ce1cfdee7b
chamroro <chamsae@handong.ac.kr>
2022-08-19 01:55:14 +0900

    update detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -7,11 +7,19 @@ function Detail(){
     const getMovies = async() => {
         const json = await(await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
         setMovie(json.data.movie);
+        console.log(json);
     };
     useEffect(() => {
         getMovies();
     }, []);
-    return <h1>{movie.title}</h1>
+    return (
+        <div>
+        <img src={movie.large_cover_image} alt={movie.title}></img>
+        <h1>{movie.title}</h1>
+        <h3>년도: {movie.year}</h3>
+        <h3>별점: {movie.rating}</h3>
+        </div>
+    );
 }
 
 export default Detail;
